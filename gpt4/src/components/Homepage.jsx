4ff9d7fbf0d3632899dd6ab8c1c8f57444153b9c
2deadmen <kartikhegde.2002@gmail.com>
2023-08-15 16:46:00 +0530

    setup

---
+++ gpt4/src/components/Homepage.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import './style.css'; // Import your stylesheet
+
+const Homepage = () => {
+  const [inputText, setInputText] = useState('');
+  const [outputText, setOutputText] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
+
+  const generateResponse = async () => {
+    if (!inputText) return;
+
+    setIsLoading(true);
+
+    try {
+      // Simulating API request delay
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      setOutputText("Generated response will be here.");
+    } catch (error) {
+      console.error('Error generating response:', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="gpt-container">
+      <h1 className="gpt-header">GPT-4 </h1>
+      <textarea
+        className="gpt-textarea"
+        rows={5}
+        cols={50}
+        placeholder="Enter your input text..."
+        value={inputText}
+        onChange={handleInputChange}
+      />
+      <button className="gpt-button" onClick={generateResponse} disabled={isLoading}>
+        Generate Response
+      </button>
+      {isLoading && <p>Loading...</p>}
+      {outputText && (
+        <div className="gpt-response">
+          <h2>Generated Response:</h2>
+          <p>{outputText}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Homepage;
